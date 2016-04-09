70a14b31803798bffeb7aca65260960982edb277
Robert Lord <robert@lord.io>
2016-04-08 22:11:16 -0500

    Add first dashboard draft

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,7 +1,7 @@
 //= require_tree .
 
 var update_timer = function() {
-  var event = new Date("2016-04-08 21:00:00 GMT-0500");
+  var event = new Date("2016-04-10 09:00:00 GMT-0500");
   var now = new Date();
   if (event - now > 0) {
     var seconds = Math.floor((event-now)/1000);
@@ -9,9 +9,9 @@ var update_timer = function() {
     seconds -= minutes * 60;
     var hours = Math.floor(minutes/60);
     minutes -= hours * 60;
-    $("#timer").text("Starting in " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
+    $("#timer").text("Coding ends in " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
   } else {
-    $("#timer").text("CarlHacks has started!");
+    $("#timer").text("Coding has stopped!");
   }
 };
 
