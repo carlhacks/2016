fc9c228a0638190605fab76caabc1a5daee39bdf
Robert Lord <robert@lord.io>
2016-04-09 14:52:34 -0500

    publish: Addl styling for large timer for dashbord
    generated from commit 10f483f350921baff8f1edec272bb9130b3857aa

--- javascripts/all-5a3368b6.js
+++ javascripts/all-5a3368b6.js
@@ -10,8 +10,10 @@ var update_timer = function() {
     var hours = Math.floor(minutes/60);
     minutes -= hours * 60;
     $("#timer").text("Coding ends in " + hours + " hours, " + minutes + " minutes, and " + seconds + " seconds");
+    $("#timer-large").html("Coding ends in<br>" + hours + " hours<br>" + minutes + " minutes<br>" + seconds + " seconds");
   } else {
     $("#timer").text("Coding has stopped!");
+    $("#timer-large").text("Coding has stopped!");
   }
 };
 
